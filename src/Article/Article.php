85ee0a338afc7666176d35da1969eeb4bc2a9a35
eliana_centamore <eliana.centamore@nubilaria.com>
2018-11-29 15:33:31 +0100

    feat(article): Add listArticle use case

---
+++ src/Article/Article.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eliana.centamore
+ * Date: 29-Nov-18
+ * Time: 3:17 PM
+ */
+
+namespace LaravelDay\Article;
+
+
+class Article
+{
+
+}
---
+++ src/Article/UseCase/ListArticles/ListArticles.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eliana.centamore
+ * Date: 29-Nov-18
+ * Time: 3:02 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class ListArticles
+{
+
+    /**
+     * ListArticles constructor.
+     */
+    public function __construct()
+    {
+    }
+}
